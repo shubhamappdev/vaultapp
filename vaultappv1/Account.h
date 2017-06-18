1e0d4d0e92fe998ee75ed022526312e00d813523
Shubham Banavalikar <shubhamappdev@gmail.com>
2017-06-18 10:23:50 -0700

    Updated folder names

---
+++ vaultappv1/Account.h
@@ -0,0 +1,21 @@
+//
+//  Account.h
+//  
+//
+//  Created by Shubham Banavalikar on 6/19/16.
+//  Copyright © 2016 Shubham Banavalikar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Account : NSObject
+
+@property (nonatomic, strong) NSString *accName;
+
+@property (nonatomic, strong) NSString *userName;
+
+@property (nonatomic, strong) NSString *passName;
+
+- (id)initWithName: (NSString *) accName;
+
+@end
---
+++ vaultappv1/EditAccountViewController.h
@@ -0,0 +1,52 @@
+//
+//  EditAccountViewController.h
+//  
+//
+//  Created by Shubham Banavalikar on 6/20/16.
+//  Copyright © 2016 Shubham Banavalikar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@class AccountsDisplayViewController;
+
+@class Account;
+
+@interface EditAccountViewController : UITableViewController <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *accCell;
+
+@property (weak, nonatomic) IBOutlet UITextField *curraccName;
+
+@property (weak, nonatomic) IBOutlet UITextField *edituserName;
+
+@property (weak, nonatomic) IBOutlet UITextField *editpassName;
+
+@property (nonatomic, retain) NSMutableData *responseData;
+
+- (IBAction)passConf:(id)sender;
+
+- (IBAction)editdoneButton:(id)sender;
+
+- (IBAction)showPass:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *changeLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *showpass;
+
+@property (nonatomic, strong) AccountsDisplayViewController *obj1;
+
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@property (weak, nonatomic) IBOutlet UITextField *passAgain;
+
+- (void) setInfo: (NSString *) befacc;
+
+@property (weak, nonatomic) IBOutlet UITextField *accURL;
+
+@property (nonatomic, strong) Account *acc1;
+
+@property (weak, nonatomic) IBOutlet UILabel *changing;
+
+@end
